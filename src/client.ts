b2a108e565357ab04b764b40819ff488c0883b67
matrushka <barisgumustas@gmail.com>
2017-11-06 21:05:42 +0100

    Updated typescript and fixed tests.

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,7 @@
 import { BigNumber } from 'bignumber.js';
-import Web3 from 'web3';
+import * as Web3 from 'web3';
 import { ICompiledContract } from './compiler';
+
 
 export interface IDeployOptions {
   contractName?: string;
