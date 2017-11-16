70e38fcc9a233ef94793a28f7efe5b1b8d626efc
matrushka <barisgumustas@gmail.com>
2017-11-16 19:57:34 +0100

    Updated web3 to 1.0.0-beta.26

--- test/contracts/SimpleStorage.test.js
+++ test/contracts/SimpleStorage.test.js
@@ -17,3 +17,20 @@ it('stores & retrieves the value with web3 fully', async () => {
   const secondValue = await contract.methods.get().call();
   expect(Number(secondValue)).toBe(10);
 });
+
+
+// TODO: Check and update depending on the open issues
+// https://github.com/ethereumjs/testrpc/issues/243
+// https://github.com/ethereum/web3.js/issues/392
+it('can sign and recover', async () => {
+  const { web3 } = helios.client;
+
+  const address = helios.client.getCurrentAccount();
+  const message = "HELLO WORLD";
+
+  const messageHash = web3.eth.accounts.hashMessage(message);
+  const signature = await web3.eth.sign(message, address);
+
+  const recoveredAddress = web3.eth.accounts.recover(messageHash, signature);
+  expect(address).toBe(recoveredAddress);
+})
